4727eb152188b6d6abb682321be8a48f49853c1b
Paulo Carmona <pcarmonaz@hotmail.com>
2010-08-10 01:37:41 -0500

    punto 12, adicion de validaciones al modelo de empresa

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,2 +1,9 @@
 class Empresa < ActiveRecord::Base
+  validates_presence_of :nit, :message => 'Este campo no puede estar vacio. '
+  validates_presence_of :nombre, :message => 'Este campo no puede estar vacio. '
+  validates_presence_of :fch_creacion, :message => 'Este campo no puede estar vacio. '
+  validates_presence_of :actividad, :message => 'Este campo no puede estar vacio. '
+  validates_presence_of :nro_empleados, :message => 'Este campo no puede estar vacio. '
+  validates_numericality_of :nro_empleados, :message => 'Debe ingresar un valor numèrico para este campo.'
+  
 end

5f756e791aac53c587cab444cc863f931caeba27
Paulo Carmona <pcarmonaz@hotmail.com>
2010-08-18 01:49:47 -0500

    puntos del 19 al 23, adicion de lineas al controlador de sucursales en todos los metodos, arreglo de vistas y rutas dentro de las vistas, adicion de link dentro del index de empresas para ingresar a las sucursales.

--- app/controllers/sucursales_controller.rb
+++ app/controllers/sucursales_controller.rb
@@ -2,7 +2,8 @@ class SucursalesController < ApplicationController
   # GET /sucursales
   # GET /sucursales.xml
   def index
-    @sucursales = Sucursal.all
+    @empresa = Empresa.find(params[:empresa_id])
+    @sucursales = @empresa.sucursales
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,7 +14,8 @@ class SucursalesController < ApplicationController
   # GET /sucursales/1
   # GET /sucursales/1.xml
   def show
-    @sucursal = Sucursal.find(params[:id])
+    @empresa = Empresa.find(params[:empresa_id])
+    @sucursal = @empresa.sucursales.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -24,7 +26,8 @@ class SucursalesController < ApplicationController
   # GET /sucursales/new
   # GET /sucursales/new.xml
   def new
-    @sucursal = Sucursal.new
+    @empresa = Empresa.find(params[:empresa_id])
+    @sucursal = @empresa.sucursales.build
 
     respond_to do |format|
       format.html # new.html.erb
@@ -34,17 +37,19 @@ class SucursalesController < ApplicationController
 
   # GET /sucursales/1/edit
   def edit
-    @sucursal = Sucursal.find(params[:id])
+    @empresa = Empresa.find(params[:empresa_id])
+    @sucursal = @empresa.sucursales.find(params[:id])
   end
 
   # POST /sucursales
   # POST /sucursales.xml
   def create
-    @sucursal = Sucursal.new(params[:sucursal])
+    @empresa = Empresa.find(params[:empresa_id])
+    @sucursal = @empresa.sucursales.build(params[:sucursal])
 
     respond_to do |format|
       if @sucursal.save
-        format.html { redirect_to(@sucursal, :notice => 'Sucursal was successfully created.') }
+        format.html { redirect_to(empresa_sucursal_path(@empresa,@sucursal), :notice => 'Sucursal was successfully created.') }
         format.xml  { render :xml => @sucursal, :status => :created, :location => @sucursal }
       else
         format.html { render :action => "new" }
@@ -56,11 +61,12 @@ class SucursalesController < ApplicationController
   # PUT /sucursales/1
   # PUT /sucursales/1.xml
   def update
+     @empresa = Empresa.find(params[:empresa_id])
     @sucursal = Sucursal.find(params[:id])
 
     respond_to do |format|
       if @sucursal.update_attributes(params[:sucursal])
-        format.html { redirect_to(@sucursal, :notice => 'Sucursal was successfully updated.') }
+        format.html { redirect_to(empresa_sucursal_path(@empresa,@sucursal), :notice => 'Sucursal actualizada Exitosamente....!') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -72,11 +78,12 @@ class SucursalesController < ApplicationController
   # DELETE /sucursales/1
   # DELETE /sucursales/1.xml
   def destroy
+    @empresa = Empresa.find(params[:empresa_id])
     @sucursal = Sucursal.find(params[:id])
     @sucursal.destroy
 
     respond_to do |format|
-      format.html { redirect_to(sucursales_url) }
+      format.html { redirect_to(empresa_sucursales_url(@empresa)) }
       format.xml  { head :ok }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
- map.resources :empresas, :has_many => sucursales
+ map.resources :empresas, :has_many => :sucursales
 
   map.resources :empresas
 
